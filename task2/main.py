caee49ecdb4f62201005c42f582f9f83249d7ec5
Nataliia Karpenko <n.karpenko@systemomega.com>
2024-01-12 07:15:49 +0100

    corrected logic task2

--- task2/main.py
+++ task2/main.py
@@ -18,19 +18,25 @@ def binary_search(arr, x):
             print(arr[mid])
         else:
             print ("Bingo!")
-            return (step, arr[mid-1])
-    return -1
+            return (step, arr[mid])
+    print("I can't find it :(")
+    return (step, arr[mid])
 
 def main():
     arr = [0.04, 0.53, 0.86, 1.23, 2.98, 3.01, 4.25, 5.31, 9.02, 11.58, 12.72, 12.77, 14.04, 16.64, 18.54, 20.31, 
           22.18, 23.54, 23.54, 24.2, 24.32, 25.28, 25.33, 25.74, 26.01, 26.61, 27.09, 28.12, 28.28, 30.5, 30.54, 31.01, 31.34, 32.52, 34.02,
           34.16, 34.22, 34.23, 37.77, 39.62, 41.04, 41.81, 42.73, 44.58, 45.02, 45.31, 46.8, 47.55, 49.75, 49.83]
-    x = 28.12
+    x = 23.54
+    print("\nI tryed to find existing value: ", x)
     result = binary_search(arr, x)
-    if result != -1:
-        print(f"I made  {result[0]} steps. \nPrevious value: {result[1]}")
-    else:
-        print("Element is not present in array")
+    print(f"I made  {result[0]} steps and found value: {result[1]}\n")
+
+    x = 44.59
+    print("\nI tryed to find not present value: ", x)
+    result = binary_search(arr, x)
+    print(f"I made  {result[0]} steps and found value: {result[1]}\n")
+    
+
 
 
 if __name__ == "__main__":
